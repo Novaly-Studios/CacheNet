533389ac2143fb17670b6961acaa6cd61dd36dd7
tpc9000 <adam.mabin@gmail.com>
2025-04-07 06:35:59 +0100

    Default definition

--- src/Test.client.lua
+++
@@ -1,8 +0,0 @@
-local CacheNet = require(script.Parent)
-    local TypeGuard = CacheNet.TypeGuard
-
-local Test = CacheNet.new("Test", TypeGuard.Array(TypeGuard.Cacheable(TypeGuard.String())))
-task.wait(2)
-Test:FireServer({"AAAA", "BBBB", "CCCC"})
-task.wait(1)
-Test:FireServer({"CCCC"})
--- src/Test.server.lua
+++
@@ -1,7 +0,0 @@
-local CacheNet = require(script.Parent)
-    local TypeGuard = CacheNet.TypeGuard
-
-local Test = CacheNet.new("Test", TypeGuard.Array(TypeGuard.Cacheable(TypeGuard.String())))
-Test.OnServerEvent:Connect(function(...)
-    print("Received", ...)
-end)
